0736e66dec87900dac460c55dbb5914dd43994ef
Matti Astrand <mattiastr@gmail.com>
2020-12-15 23:30:54 +0000

    Faster day 15, use array instead of hashmap

--- src/day15.rs
+++ src/day15.rs
@@ -1,19 +1,18 @@
-use std::collections::HashMap;
 use std::io;
 
 pub fn part1(n: usize) -> io::Result<i64> {
-    let mut v: HashMap<usize, usize> = [5, 1, 9, 18, 13, 8, 0]
-        .iter()
-        .enumerate()
-        .map(|(i, j)| (*j, i))
-        .collect();
+    let input: Vec<usize> = vec![5, 1, 9, 18, 13, 8, 0];
+    let mut v: Vec<Option<usize>> = vec![None; n];
+
+    for (i, &x) in input.iter().enumerate() {
+        v[x] = Some(i);
+    }
 
     let mut last = 0;
-    for i in v.len()..n - 1 {
-        let jutska = v.get(&last).map_or(0, |j| i - j);
-        v.insert(last, i);
+    for i in input.len()..n - 1 {
+        let jutska = v[last].map_or(0, |j| i - j);
+        v[last] = Some(i);
         last = jutska;
     }
-    println!("size {}", v.len());
     Ok(last as i64)
 }
