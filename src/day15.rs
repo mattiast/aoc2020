84fdcd1cecd55ee1b5205f6296bdc0c661d121de
Matti Astrand <mattiastr@gmail.com>
2020-12-15 23:22:09 +0000

    Day 15 done

--- src/day15.rs
+++ src/day15.rs
@@ -1,11 +1,19 @@
+use std::collections::HashMap;
 use std::io;
+
 pub fn part1(n: usize) -> io::Result<i64> {
-    let mut v: Vec<usize> = vec![5, 1, 9, 18, 13, 8, 0];
-    v.reserve(n);
-    for i in v.len()..n {
-        let last = v[i - 1];
-        let jutska = (1..=i - 1).find(|j| v[i - 1 - j] == last).unwrap_or(0);
-        v.push(jutska);
+    let mut v: HashMap<usize, usize> = [5, 1, 9, 18, 13, 8, 0]
+        .iter()
+        .enumerate()
+        .map(|(i, j)| (*j, i))
+        .collect();
+
+    let mut last = 0;
+    for i in v.len()..n - 1 {
+        let jutska = v.get(&last).map_or(0, |j| i - j);
+        v.insert(last, i);
+        last = jutska;
     }
-    Ok(v[n - 1] as i64)
+    println!("size {}", v.len());
+    Ok(last as i64)
 }
